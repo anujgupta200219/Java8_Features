2e935538d6a5342acc0527c098634e84ae9c7b83
Anuj <anuj.20bce7055@vitap.ac.in>
2025-06-08 21:17:05 +0530

    Learning_Java8_features

--- src/main/java/org/example/Comparator.java
+++ src/main/java/org/example/Comparator.java
@@ -1,13 +1,16 @@
 package org.example;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
+import java.util.*;
 
 public class Comparator {
     public static void main(String args[]){
-        ArrayList<Integer>arr=new ArrayList<>(Arrays.asList(6,3,5,2,8,9));
-        Collections.sort(arr,(a,b)->b-a);
-        System.out.println(arr);
+//        ArrayList<Integer>arr=new ArrayList<>(Arrays.asList(6,3,5,2,8,9));
+//        Collections.sort(arr,(a,b)->b-a);
+//        System.out.println(arr);
+        Map<Integer,String>map=new TreeMap<>((a,b)->b-a);
+        map.put(2,"def");
+        map.put(1,"abc");
+        map.put(3,"hij");
+        System.out.println(map);
     }
 }
